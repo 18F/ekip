60e296ad5c17cd88bd60c092f486850837535fda
Sean Herron <sean.herron@gsa.gov>
2015-05-26 18:07:51 -0400

    a lot of theme things

--- assets/js/ground.js
+++ assets/js/ground.js
@@ -1,16 +1,28 @@
-$(document).ready(function() {
+function drawTrees() {
 
-	var numberOfTrees = Math.floor(Math.random()*(40-25+1)+25);
+	var windowWidth = $(window).width();
+	
+	var numberOfTrees = windowWidth * 0.015;
+	console.log(numberOfTrees);
 
 	var treeContainer = $('.ground')
 
+	var treePositions = []
+
 	for (i = 0; i < numberOfTrees; i++) {
 		var treeColor = Math.floor(Math.random()*(6-1+1)+1);
-		var treePositionLeft = Math.floor(Math.random()*(99)+1);
-		var treePositionTop = Math.floor(Math.random()*(75)+5);
+		var treePositionLeft = Math.floor(Math.random()*(75)+1);
 
-		var tree = $('<div class="object__tree color-' + treeColor + '" style="margin-left:' + treePositionLeft + '%; top:' + treePositionTop + '%;"></div>')
+		var treePositionTop = Math.floor(Math.random()*(40)+9);
+
+		var tree = $('<div class="object__tree color-' + treeColor + '" style="left:' + treePositionLeft + '%; top:' + treePositionTop + '%;"></div>')
     	treeContainer.append(tree);
 	}
+	console.log(treePositions);
+}
+
+$(document).ready(function() {
+
+	drawTrees();
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp'),
     path = require('path'),
     batch = require('gulp-batch'),
+    plumber = require('gulp-plumber'),
 
     // SCSS
     sass = require('gulp-sass'),
  
     // JS BUILD
     uglify = require('gulp-uglify'),
+
     // Exec
     exec = require('child_process').exec,
 
     // Publishing
     s3 = require('gulp-s3');
+    awspublish = require('gulp-awspublish');
+
 
     // Import files
     pkg = require('./package.json')
@@ -20,8 +24,6 @@ var fs = require('fs');
 
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-
-aws = JSON.parse(fs.readFileSync('./aws-staging.json'));
 
 gulp.task('jekyll-build', function (done) {
   browserSync.notify('<span style="color: grey">Running:</span> $ jekyll build');
@@ -34,17 +36,21 @@ gulp.task('jekyll-build', function (done) {
 
 gulp.task('jekyll', ['jekyll-build'], function() {
     gulp.src('assets/js/*.js')
+        .pipe(plumber())
         //.pipe(uglify())
         .pipe(gulp.dest('_deploy/assets/js'));
 
     gulp.src('bower_components/jquery/dist/jquery.min.js')
+        .pipe(plumber())
         .pipe(gulp.dest('_deploy/assets/js'));
 
     gulp.src('assets/img/*')
+        .pipe(plumber())
         .pipe(gulp.dest('_deploy/assets/img'))
         .pipe(reload({stream:true}));
 
     return gulp.src('assets/scss/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('_deploy/assets/css'))
         .pipe(reload({stream:true}));
@@ -53,6 +59,7 @@ gulp.task('jekyll', ['jekyll-build'], function() {
 
 gulp.task('scss', function() {
     return gulp.src('assets/scss/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('_deploy/assets/css'))
         .pipe(reload({stream:true}));
@@ -60,12 +67,14 @@ gulp.task('scss', function() {
 
 gulp.task('images', function() {
     return gulp.src('assets/img/*')
+        .pipe(plumber())
         .pipe(gulp.dest('_deploy/assets/img'))
         .pipe(reload({stream:true}));
 });
 
 gulp.task('compress-js', function() {
     return gulp.src('assets/js/*.js')
+        .pipe(plumber())
         //.pipe(uglify())
         .pipe(gulp.dest('_deploy/assets/js'))
         .pipe(reload({stream:true}));
@@ -73,19 +82,27 @@ gulp.task('compress-js', function() {
 
 gulp.task('jquery', function() {
     return gulp.src('bower_components/jquery/dist/jquery.min.js')
+        .pipe(plumber())
         .pipe(gulp.dest('_deploy/assets/js'));
 });
 
 gulp.task('publish-staging', ['jekyll'], function() {
-    var options = { headers: {'Cache-Control': 'max-age=5, no-transform, public'} }
+    var aws = JSON.parse(fs.readFileSync('./aws-staging.json'));
+    var publisher = awspublish.create(aws)
+    var headers = { headers: {'Cache-Control': 'max-age=5, no-transform, public'} }
     return gulp.src('./_deploy/**')
-        .pipe(s3(aws, options));
+        .pipe(plumber())
+        .pipe(awspublish.gzip({ ext: '.gz' }))
+        .pipe(publisher.publish(headers))
+        .pipe(publisher.cache())
+        .pipe(awspublish.reporter());
 });
 
 gulp.task('publish-production', ['jekyll'], function() {
     aws = JSON.parse(fs.readFileSync('./aws-production.json'));
-    var options = { headers: {'Cache-Control': 'max-age=5, no-transform, public'} }
+    var headers = { 'Cache-Control': 'max-age=5, no-transform, public' }
     return gulp.src('./_deploy/**')
+        .pipe(plumber())
         .pipe(s3(aws, options));
 });
 
